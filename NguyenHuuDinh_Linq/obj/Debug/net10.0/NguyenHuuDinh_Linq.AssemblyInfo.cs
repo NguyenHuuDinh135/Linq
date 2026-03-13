dc0617fc5642cd52fbf3f491c318da3bafe66f20
NguyenHuuDinh <dinhknd3@gmail.com>
2026-03-13 13:06:59 +0700

    Reset to initial template

--- NguyenHuuDinh_Linq/obj/Debug/net10.0/NguyenHuuDinh_Linq.AssemblyInfo.cs
+++ NguyenHuuDinh_Linq/obj/Debug/net10.0/NguyenHuuDinh_Linq.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NguyenHuuDinh_Linq")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c24059d310a2525f679ddfd9eb4c620680c8cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("NguyenHuuDinh_Linq")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NguyenHuuDinh_Linq")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
